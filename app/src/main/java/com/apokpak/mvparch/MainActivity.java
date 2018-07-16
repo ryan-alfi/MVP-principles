7fc4816122314578fb424449fc3527c1898e968b
Ryan Alfi <real.rezz@gmail.com>
2018-07-16 15:04:46 +0700

    commit

---
+++ app/src/main/java/com/apokpak/mvparch/MainActivity.java
@@ -0,0 +1,62 @@
+package com.apokpak.mvparch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements MainView {
+    private TextView textView;
+    private Button button;
+    private ProgressBar progressBar;
+    MainPresenter mainPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mainPresenter = new MainPresenterImpl(this, new GetQuoteInteractorImpl());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainPresenter.onButtonClick();
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mainPresenter.onDestroy();
+    }
+
+
+    @Override
+    public void showProgress() {
+        progressBar.setVisibility(View.VISIBLE);
+        textView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void hideProgress() {
+        progressBar.setVisibility(View.GONE);
+        textView.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void setQuote(String string) {
+        textView.setText(string);
+    }
+}
---
+++ app/src/main/java/com/apokpak/mvparch/MainView.java
@@ -0,0 +1,13 @@
+package com.apokpak.mvparch;
+
+/**
+ * Created by ryanalfi on 16/07/18.
+ */
+
+public interface MainView {
+    void showProgress();
+
+    void hideProgress();
+
+    void setQuote(String string);
+}
